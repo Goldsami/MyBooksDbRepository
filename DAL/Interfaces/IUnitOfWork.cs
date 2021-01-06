e1ae4f633fa20ee407368f7775d39c7ea360c07b
Masksym <max@devmiles.com>
2021-01-06 22:03:55 +0200

    Added Repository and UoW interfaces

---
+++ DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    interface IUnitOfWork: IDisposable
+    {
+        IRepository<Genre> Genres { get; }
+        IRepository<Book> Books { get; }
+        IRepository<Author> Authors { get; }
+        IRepository<User> Users { get; }
+        IRepository<BooksList> BooksLists { get; }
+
+        Task SaveAsync();
+    }
+}
